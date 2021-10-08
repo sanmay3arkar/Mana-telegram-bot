ebb209f13568d0731ed4d21fca67d1afe67ff333
Sanmay <49298843+sanmay3arkar@users.noreply.github.com>
2021-10-08 11:00:18 +0530

    Add files via upload

---
+++ Kushida_Bot/modules/helper_function/list_dict.py
@@ -0,0 +1,59 @@
+BACK_AFK = [
+'{} is back with a bang!!',
+'Welcome back {} want cofee☕',
+'{} Is back! now one more member increased!!',
+'{} is here!',
+'{} is now in the chat after all things over!',
+'{} is finnaly here after such a long time!',
+'Be gentle with all {}!',
+'How {} Back alive!!',
+"thanks to God you haven't died yet {}🥺","don't be crazy{}🤪",
+'WoW!! You alive {} i thought you were dead!',
+'Good to have you back {}😊']
+
+AWAY = ['{} is Now fallen into a deep sleep😴!!',
+'Good bye until next time {}👋 ',
+'{} Is now away...!',
+'Everyone will be waiting for you {}🥺!',
+'See you soon {}😊 ',
+'‌{} Is now away.. for a Little longer',
+"Bye! {}.. but please don't die ☠",
+'{} is gone now....!',
+"Promise🤙.. you'll come back my friend {}",
+'{} will come back later..']
+
+REPLY_AWAY = ['{} will not be back for a while.\n please wait untill that😊\n\n',
+'{} is currently away from keyboard!\n\n',
+'It feels Lonely without him. but the truth is {} is now away😔\n\n',
+'{} has done with this group for a while😹\n\n',
+'{} is away from keyboard!\n\n',
+'{} is afk for a while.\n you can talk with me or everyone else on this group if you want😊\n\n']
+
+TELE = ['1200','1250','1300', '1350', '1400', '1450', '1500', '1550', '1600', '1650', '1700', '1750', '1800', '1850', '1900', '1950', '2000', '2050', '2100', '2150', '2200', '2250', '2300']
+
+DAYCLEAN = ['sunny1.jpg', 'sunny2.jpg', 'sunny3.jpg', 'sunny4.jpg', 'sunny5.jpg']
+
+DAYRAIN = ['rain1.jpg', 'rain2.jpg', 'rain3.jpg', 'rain4.jpg', 'rain5.jpg']
+
+DAYSNOW = ['snow1.jpg', 'snow2.jpg', 'snow3.jpg', 'snow4.jpg', 'snow5.jpg']
+
+DAYCLOUDY = ['full_clowdy1.jpg', 'full_clowdy2.jpg', 'full_clowdy3.jpg', 'full_clowdy4.jpg', 'full_clowdy5.jpg']
+
+DAYHFCLOUD = ['Partly_clowdy1.jpg', 'partly_clowdy2.jpg', 'partly_clowdy3.jpg',
+'partly_clowdy4.jpg', 'partly_clowdy5.jpg']
+
+NIGHTCLEAN = ['night_clean1.jpg', 'night_clean2.jpg', 'night_clean3.jpg', 'night_clean4.jpg', 'night_clean5.jpg']
+
+NIGHTRAIN = ['night_rain1.jpg', 'night_rain2.jpg', 'night_rain3.jpg', 'night_rain4.jpg', 'night_rain5.jpg']
+
+NIGHTSNOW = ['night_snow1.jpg', 'night_snow2.jpg', 'night_snow3.jpg', 'night_snow4.jpg', 'night_snow5.jpg']
+
+NIGHTCLOUD = ['night_clowdy1.jpg', 'night_clowdy2.jpg', 'night_clowdy3.jpg', 'night_clowdy4.jpg', 'night_clowdy5.jpg']
+
+NIGHTHFCLOUD = ['half_nightclowd1.jpg', 'half_nightclowd2.jpg', 'half_nightclowd3.jpg', 'half_nightclowd4.jpg', 'half_nightclowd5.jpg']
+
+FOG = ['fog1.jpg', 'fog2.jpg', 'fog3.jpg', 'fog4.jpg', 'fog5.jpg']
+
+STORM = ['storm1.jpg', 'storm2.jpg', 'storm3.jpg', 'storm4.jpg', 'storm5.jpg']
+
+WARN = ['You are now warned by our admis for your wrong work.', 'Be quite!! You are currently warned by our admins.', 'You Are warned by our admins if you do same thing again you will be banned from this group.', 'You did a wrong thing for that reason you are warned by our admins.']
