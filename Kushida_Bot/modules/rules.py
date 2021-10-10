a98ff194cef4b5a046dd2577609359014743b94d
Sanmay <49298843+sanmay3arkar@users.noreply.github.com>
2021-10-10 19:42:46 +0530

    Update rules.py

--- Kushida_Bot/modules/rules.py
+++ Kushida_Bot/modules/rules.py
@@ -1,12 +1,11 @@
 from telegram import Bot, ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater
-from Kushida_Bot.modules.console import rle
 
 bot = Bot("1929797199:AAFLk4hTedHqMiL7sddYhFqWJtqtJKTeiBA")
 
 updates = Updater("1929797199:AAFLk4hTedHqMiL7sddYhFqWJtqtJKTeiBA", use_context = True)
 
-rules = rle
+rules = {}
 btns = {}
 btn_res = {}
 lnks = {}
